919547d54683d6964366c74a3f3500afa8296fe1
Yanis Renard-dumont <74371878+Alweare@users.noreply.github.com>
2024-07-23 13:45:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { request } = require("http");
+const { resolve } = require("path");
+
 
 const app = express();
+// Autoriser express à recevoir des données envoyées en JSON dans le body(payload)
+app.use(express.json());
 // MOCK
 // Simulation de données en mémoire
 let DB_ARTICLES = [
@@ -24,42 +28,52 @@ let DB_ARTICLES = [
 		author: "Toto",
 	},
 ];
-app.use(express.json());
+
 app.get("/articles", (request, response) => {
 	return response.json(DB_ARTICLES);
 });
 
 app.get("/articles/:id", (request, response) => {
 	const articleID = parseInt(request.params.id);
-	let listeArticles = DB_ARTICLES.find(a => a.id === articleID);
+	let listeArticles = DB_ARTICLES.find((a) => a.id === articleID);
 	return response.json(listeArticles);
 });
 
 app.post("/save-article", (request, response) => {
-
-	let article = request.body;
-	let articleFound = false;
-
-	for(let i = 0 ; i< DB_ARTICLES.length;i++){
-		if(article){
-			if(article.id === DB_ARTICLES[i].id){
-				DB_ARTICLES[i] = article;
-				articleFound = true;
-			}
-		}
+	//Récupérer l'article en JSON
+	const articleJSON = request.body;
+	let foundArticle = null;
+	// essayer de trouver un article existant
+	if (articleJSON.id != undefined || articleJSON.id ) {
+		foundArticle = DB_ARTICLES.find(article => article.id === articleJSON.id);
 	}
-		if(!articleFound){
-			DB_ARTICLES.push(article);
-			return response.json({message : "ajouté"})
-		}else {
-			return response.json({ message: "modifié" });
-		}
+	// Si je trouve je modifie les nouvelles
+	if (foundArticle) {
+		foundArticle.title = articleJSON.title;
+		foundArticle.content = articleJSON.content;
+		foundArticle.author = articleJSON.author;
 
-	});
+		return response.json(`L'article a été modifié avec succès`);
+	}
+	// Sinon par défaut je créer
+	DB_ARTICLES.push(articleJSON);
+	return response.json(`article crée avec succès`);
+});
+
 app.delete("/articles/:id", (request, response) => {
+	// Il faut l'id en entier
 	const idADelete = parseInt(request.params.id);
-	DB_ARTICLES.splice(idADelete, 1);
-	return response.json({message : "deleted"})
+
+	const foundArticleIndex = DB_ARTICLES.find(article => article.id === articleJSON.id);
+
+	// si article  trouve erreur
+	if(foundArticleIndex <0){
+		return response.json(`Impossible de supprimer un article inexistant`);
+	}
+
+
+	DB_ARTICLES.slice(idADelete, 1);
+	return response.json({ message: "deleted" });
 });
 
 // Démarrer le serveur
